33c746ae7f43fa6803e0ba71221023d3599bd30a
Rafael Flora <rafaelflora.design@gmail.com>
2022-02-13 21:10:59 -0500

    HTML and JS updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var randomStrings = [
     "The way you laugh.",
     "Your heart shaped nostrils.",
-    "The warmth of your hugs.",
+    "The warmth ßof your hugs.",
     "Our thermodynamic equilibrium.",
     "How perfectly your beauty marks are placed on your face.",
     "The face you make when you see a cute cat.",
@@ -20,7 +20,14 @@ var randomStrings = [
     "How you have levelled up my fashion sense.",
     "Your 'British' accent.",
     "How excited you get when we go to a stationery store.",
-
+    "The sweet sound of your voice when you sing, even though you think you can't",
+    "Someone I can laugh so honestly with, someone who can make me laugh so much.",
+    "Every day I wake up am grateful that I look forward to talking to you. Yes, even when I may seem sleepy or grumpy, I am happy to hear your voice. I am happy to see your face, to have someone so beautiful in my life.",
+    "You are someone who feels like home, who makes the world feel like it is worth going through, even when I feel hopeless.",
+    "That we can eat, and the moments we share just by sharing meals.",
+    "Your smile that is so genuine and so warm, that it can make any day of mine feel like it was good, even when it was not.",
+    "How you are someone that loves me so honestly. Someone who loves me even when I feel I am not deserving of it, who reminds me that I am loved.",
+    "How you are someone who helps me grow and someone who helps me to become the best version of myself.",
   ];
 
   var randomDiv = document.getElementById("myRandomDiv");
